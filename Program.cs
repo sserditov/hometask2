7808d50a3391665e60fb156c67dde2761dd92409
sserditov <108936913+sserditov@users.noreply.github.com>
2022-08-09 15:43:08 +0500

    hometask2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число a ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("Максимальное число " + a);
+}
+else
+{
+    Console.WriteLine("Максимальное число " + b);
+}
